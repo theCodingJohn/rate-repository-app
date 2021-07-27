dfe31b7728e08e204b6681337beb3196ff949290
John Carlo Cunanan <codingjohn@outlook.com>
2021-07-27 15:57:29 +0800

    Add exercise 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Pressable } from "react-native";
+import { View, StyleSheet, Pressable, ScrollView } from "react-native";
 import Constants from "expo-constants";
 import { Link } from "react-router-native";
 
@@ -14,28 +14,30 @@ const styles = StyleSheet.create({
     backgroundColor: "#24292e",
   },
 
-  repository_btn: {
+  nav_btn: {
     marginRight: 10,
   },
 });
 
 const AppBar = () => {
   return (
-    <View style={styles.container}>
-      <Pressable style={styles.repository_btn}>
-        <Link to="/">
-          <Text color="light" fontSize="heading" fontWeight="bold">
-            Repositories
-          </Text>
-        </Link>
-      </Pressable>
-      <Pressable>
-        <Link to="/signin">
-          <Text color="light" fontSize="heading" fontWeight="bold">
-            Signin
-          </Text>
-        </Link>
-      </Pressable>
+    <View>
+      <ScrollView style={styles.container} horizontal>
+        <Pressable style={styles.nav_btn}>
+          <Link to="/">
+            <Text color="light" fontSize="heading" fontWeight="bold">
+              Repositories
+            </Text>
+          </Link>
+        </Pressable>
+        <Pressable style={styles.nav_btn}>
+          <Link to="/signin">
+            <Text color="light" fontSize="heading" fontWeight="bold">
+              Signin
+            </Text>
+          </Link>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 };
